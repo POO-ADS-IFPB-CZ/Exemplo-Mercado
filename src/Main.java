94c628645e60b381a244c72e59d968874f4b1bca
Paulo Freitas <paulo.freitas.nt@gmail.com>
2024-03-04 09:48:45 -0300

    Versão inicial

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Produto produto = new Produto("Arroz", 5,
+                LocalDate.of(2024,6,3));
+        Produto produto2 = new Produto("Feijão", 10,
+                LocalDate.of(2024,6,3));
+        Produto produto3 = new Produto("Açucar", 4,
+                LocalDate.of(2024,6,3));
+
+        System.out.println(produto.getCodigo());
+        System.out.println(produto2.getCodigo());
+        System.out.println(produto3.getCodigo());
+
+
+    }
+}
---
+++ src/Produto.java
@@ -0,0 +1,60 @@
+import java.time.LocalDate;
+
+public class Produto {
+
+    private static int contador;
+    private final int codigo;
+    private String descricao;
+    private float valor;
+    private LocalDate validade;
+    private int estoque;
+
+    public Produto(String descricao, float valor,
+                   LocalDate validade){
+        codigo = ++contador;
+        this.descricao = descricao;
+        this.valor = valor;
+        this.validade = validade;
+        estoque = 0;
+    }
+
+    public static int getContador(){
+        return contador;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+    public LocalDate getValidade() {
+        return validade;
+    }
+
+    public void setValidade(LocalDate validade) {
+        this.validade = validade;
+    }
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
+}
