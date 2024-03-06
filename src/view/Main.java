41f236cc6be0b997e50b9cfb0e09fe0a681f5192
Paulo Freitas <paulo.freitas.nt@gmail.com>
2024-03-06 07:53:35 -0300

    Adicionando menu para cadastrar produto

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,16 +1,43 @@
 package view;
 
-import java.time.LocalDate;
+import model.Produto;
+
 import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         DateTimeFormatter formatter = DateTimeFormatter
                 .ofPattern("dd/MM/yyyy");
-        String data = "03/04/2024";
-        LocalDate localDate = LocalDate.parse(data, formatter);
-        System.out.println(localDate.format(formatter));
+        Scanner scanner = new Scanner(System.in);
+        Produto produto;
 
+        while (true){
+            System.out.println("Escolha a opção:" +
+                    "\n 1 - Para cadastrar Produto" +
+                    "\n 2 - Para exibir Produto" +
+                    "\n 3 - Para atualizar Produto" +
+                    "\n 4 - Para remover Produto" +
+                    "\n 5 - Para sair");
+            int escolha = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (escolha){
+                case 1:
+                    cadastrarProduto();
+                    break;
+                case 5:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Opção Inválida");
+            }
+
+        }
+
+    }
+
+    private static void cadastrarProduto() {
     }
 }
